042334ac5e27293c07f514aa3f9d5fc7ecd9f8e8
hulamo <hugoladislao@gmail.com>
2019-07-10 23:20:32 -0600

    Updates

---
+++ precache-manifest.764035c50047d602c1bcd58b29f476bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b33d39aa38a5c7f712f28a7c3ba9a0",
+    "url": "/memorama/index.html"
+  },
+  {
+    "revision": "0f312545e21e66a4cbd3",
+    "url": "/memorama/static/css/main.dba943a5.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/memorama/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "0f312545e21e66a4cbd3",
+    "url": "/memorama/static/js/main.14b1acf5.chunk.js"
+  },
+  {
+    "revision": "b54470dc4e3e2650f6ec",
+    "url": "/memorama/static/js/runtime~main.b56c1681.js"
+  }
+]);
